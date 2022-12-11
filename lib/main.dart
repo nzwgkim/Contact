f03c426746f9c0a1592b2e2cdfa600d1311004dc
Woody <nz.wgkim@gmail.com>
2022-12-12 08:38:12 +1300

    body and bottomNavigationBar using Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,38 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-        home: Center(
-          child: Container(width: 50,height: 50,color: Colors.blue)
+        home: Scaffold(
+          body: Container (
+            color: Colors.grey,
+            height : 400,  //높이 넣어야 세로정렬 가능할듯
+            child : Row (
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Icon(Icons.star),
+                Icon(Icons.star),
+                Icon(Icons.star),
+              ],
+            ),
+          ),
+            bottomNavigationBar:BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.phone),
+                  label: 'phone',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.message),
+                  label: 'message',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.contact_page),
+                  label: 'contact_page',
+                ),
+              ],
+            ),
         )
-      //Container(width: 50,height: 50,color: Colors.blue)
     );
   }
 }
